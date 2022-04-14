d0e014c306e8ab0aac785d9c22f3e7fe2650ceea
lauro <lauropimentel12@gmail.com>
2022-04-14 20:33:39 -0300

    cookie false

--- btax/settings.py
+++ btax/settings.py
@@ -173,4 +173,5 @@ REST_FRAMEWORK = {
 
 # django-cors-headers
 CORS_ORIGIN_ALLOW_ALL = True
+CSRF_COOKIE_HTTPONLY = False
 CSRF_TRUSTED_ORIGINS = ['.bitrix24.com.br','https://beytrix.bitrix24.com.br', 'https://beytrix.bitrix24.com.br/', 'https://btaxtest.herokuapp.com/','https://btaxtest.herokuapp.com']
