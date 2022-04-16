a73a206709b2610c42533691bad117517c37e219
lauro <lauropimentel12@gmail.com>
2022-04-16 10:04:13 -0300

    post test

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from django.http import HttpResponse
 from . import views
 
 app_name='api'
 
+
+
+def testre(request):
+    print(request.POST)
+    print(request)
+    print(request.json())
+    print(request.POST.items())
+    return HttpResponse(request.POST.items())
 urlpatterns = [
-    path('cobrancas/emitir/', views.CobrancaEmitir.as_view(), name='cobrancas-emitir'),
+    #path('cobrancas/emitir/', views.CobrancaEmitir.as_view(), name='cobrancas-emitir'),
+    path('cobrancas/emitir/', testre, name='cobrancas-emitir'),
     path('cobrancas/consultar/', views.CobrancaConsulta.as_view(), name='cobrancas-consulta'),
     
     path('boletos/recebe-notificacao/', views.BoletoRecebeNotificacao.as_view(), name='boletos-recebe-notificacao'),
