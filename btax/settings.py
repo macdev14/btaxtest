4ea85a35b28795bb93d77b44e1d5c1dd2b09fe1e
lauro <lauropimentel12@gmail.com>
2022-05-05 16:56:24 -0300

    teste

--- btax/settings.py
+++ btax/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_r-ih4p-*&82+gn=!rr(13@72^%q1_k9&8h3ne$1eo79$l4=h^'
 
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(' ')
+
+AWS_URL = os.environ['AWS_URL']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_URL = os.environ['AWS_URL']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG'] if 'DEBUG' in os.environ else False
