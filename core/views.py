d2d08591aaef7c8fe8191f698c0666505a1e9f80
lauro <lauropimentel12@gmail.com>
2022-05-09 11:53:55 -0300

    btax bx24

--- core/views.py
+++ core/views.py
@@ -75,11 +75,12 @@ def schedule_refresh():
 @csrf_exempt
 def boleto_url_update(request, id_negocio):
     print('ran')
-    if id_negocio:  
+    resp = redirect('core:home')
+    if id_negocio :  
         PREFIX = 'boletos/'
         url_boleto = static('assets/'+PREFIX+f'boleto_{id_negocio}.pdf')
-        request.set_cookie('id_negocio', id_negocio)
-        request.set_cookie('url_boleto', url_boleto)
+        resp.set_cookie('id_negocio', id_negocio)
+        resp.set_cookie('url_boleto', url_boleto)
         res = bx24.call('crm.deal.update', { 'id': id_negocio,  'fields':{'UF_CRM_1643650856094': url_boleto }} )
         if 'error' in res:
             resp = redirect('core:home')
@@ -88,10 +89,13 @@ def boleto_url_update(request, id_negocio):
     if 'id_negocio' in request.COOKIES and 'url_boleto' in request.COOKIES and request.COOKIES['id_negocio'] and request.COOKIES['url_boleto']:
         res = bx24.call('crm.deal.update', { 'id': request.COOKIES['id_negocio'],  'fields':{'UF_CRM_1643650856094': request.COOKIES['url_boleto'] }} )
         print(res)
+        
         if 'error' in res:
             resp = redirect('core:home')
             resp.set_cookie('VIEW_REDIRECT', 'core:boleto-url-update')
             return resp
+        resp.delete_cookie('id_negocio', id_negocio)
+        resp.delete_cookie('url_boleto', url_boleto)
     return redirect('core:home')
     
 #@bitrix_auth(bx24)
