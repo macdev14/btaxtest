35233e307b3c6bac792718a8e2242f1fb161d333
lauro <lauropimentel12@gmail.com>
2022-05-09 10:13:38 -0300

    btax bx24

--- boletos/views.py
+++ boletos/views.py
@@ -35,12 +35,16 @@ def consultar_boleto(request):
 #     resp = HttpResponseRedirect(reverse('core:update-btax'))
 #     #resp.set_cookie('token', token)
 #     resp.set_cookie('VIEW_REDIRECT', 'core:url-boleto-btax')
-
+@login_required
+def boletos_delete_all(request):
+    querys.update_varios_objs(Boleto.COLLECTION_NAME, {'cedente_cpf_cnpj': str(request.user.profile.conta.cpf_cnpj), 'situacao' : 'SALVO' }, { '$set':{ 'situacao' : 'FALHA' }  }  )
+    resp = HttpResponseRedirect(reverse('boletos:boletos-gerados'))
+    return resp
 @login_required
 def boletos_gerados(request):
     print(str(request.user.profile.conta.cpf_cnpj))
     boletos = list(querys.filtra_objs(Boleto.COLLECTION_NAME, {'cedente_cpf_cnpj': str(request.user.profile.conta.cpf_cnpj), 'situacao' : 'SALVO' }  ))
-    querys.update_varios_objs(Boleto.COLLECTION_NAME, {'cedente_cpf_cnpj': str(request.user.profile.conta.cpf_cnpj), 'situacao' : 'SALVO' }, { '$set':{ 'situacao' : 'FALHA' } })
+  
     #boletos_gerados = {}
     for result_object in boletos:
          print(result_object)
